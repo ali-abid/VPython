cc93588990f5698e7044246b1881bb8e0bffad6c
ali-abid <worktimee@gmail.com>
2015-03-05 14:05:31 +0000

    X Y Z Velocity calculated by formula.e.g deltaX/deltaT

--- calXYZvelocity.py
+++ calXYZvelocity.py
@@ -27,13 +27,32 @@ vscale = 0.1
 #initial time and time interval
 t= 0
 deltaT = 0.07
+
+#Reading x y and z coordinates from .csv file
+for line in reader:
+    x.append(line[1]),y.append(line[2]),z.append(line[3])
+
+#Following three loops calculating x y and z velocity. e.g X velocity = deltaX/deltaY
+for i in range(len(x)-1):
+    deltax = float(x[i+1]) - float(x[i])#calculate displacement
+    x[i+1] = deltax #store delatax as inital point in x[i+1]
+    vX.append(deltax/deltaT)# store velocit deltatX/deltaT inside vX[]
+
+for i in range(len(y)-1):
+    deltay = float(y[i+1]) - float(y[i]) # substact from y final point to y inital point
+    y[i+1] = deltay # Store deltay value as y inial point.
+    vY.append(deltay/deltaT)#Store y velocity into vY[]
+
+for i in range(len(z)-1):
+    deltaz = float(z[i+1]) - float(z[i])
+    z[i+1] = deltaz
+    vZ.append(deltaz/deltaT)
+
 #Creating ball sphere
 ball2 = sphere(pos = vector(0,0,0), radius = 0.1, color=color.blue)
 #Creating ball trail
 ball2.trail = curve(color=ball2.color)
-#Reading x y and z coordinates from .csv file
-for line in reader:
-    x.append(line[1]),y.append(line[2]),z.append(line[3])
+
 #Label x y and z coordinates
 xaxis=cylinder(color=color.red, pos=vector(0,0,0), axis=vector(2,0,0), radius=0.2)
 xlbl=label(pos=vector(3,0,0), text="X", color=color.red, opacity=0, height=30, box=0)
@@ -43,20 +62,21 @@ zaxis=cylinder(color=color.blue, pos=(0,0,0), axis=(0,0,2), radius=0.2)
 xlbl=label(pos=vector(0,0,3), text="Z", color=color.blue, opacity=0, height=30, box=0)
 #Create vector arrow
 r = arrow(pos=vector(0,0,0), axis = vector(0,0,0), color=color.white, shaftwidth=0.1)
-#Run loop read all index values from x[] y [] z[] arrays, in this code all velocity calculated by ball.velocity object  
-for i in range(len(x)-1):
+
+#Read calculated velocity values from vX[] vY[] vZ[] arrays and move ball positions  
+for i in range(len(vX)-1):
     #No more than 10 loop per second
     rate(40)
-    #Reade ball velocity from vX, vY and vZ arrays
-    ball2.velocity = vector(float(x[i]),float(y[i]),float(z[i]))*vscale
+    #Read ball velocity from vX, vY and vZ arrays
+    ball2Velocity = vector(float(x[i]),float(y[i]),float(z[i]))*vscale
     #Draw arrow along ball velocity
-    r.pos= ball2.velocity*vscale
+    r.pos= ball2Velocity*vscale
     r.axis = ball2.pos
-    ra = arrow(pos= ball2.pos, axis = ball2.velocity*vscale, color=color.yellow)
+    ra = arrow(pos= ball2.pos, axis = ball2Velocity*vscale, color=color.yellow)
     #Update ball position 
-    ball2.pos = ball2.pos + ball2.velocity*deltaT
+    ball2.pos = ball2.pos + ball2Velocity*deltaT
     #Draw trail along ball movement
     ball2.trail.append(pos=ball2.pos)
-    print(ball2.velocity)
+    print(ball2Velocity)
 #file close
 file.close()
