a9e6648c994b61277aa7268bad3017520c47e6ca
ali-abid <worktimee@gmail.com>
2015-04-15 14:36:19 +0100

    Define Getter & Setter method

--- convert_MPU6050_AccGyro_RawData.py
+++ convert_MPU6050_AccGyro_RawData.py
@@ -17,7 +17,7 @@ from visual.graph import *
 import time
 from visual.filedialog import save_file
 
-#Declare Arrays
+#DECLARE ARRAYS
 Tnum = []
 Xnum = []
 Ynum = []
@@ -38,4 +38,52 @@ angle_x_data = []
 angle_y_data = []
 angle_z_data = []
 
-#Define Sensor Calibration Method.
+#DECLARE GLOBAL VARIABLES, FOR HOLDING ROTATION ANGLE OF SENSOR (ACC & GYRO)
+last_read_time = 0
+last_x_angle = 0                            #These are the filtered angles
+last_y_angle = 0
+last_z_angle = 0  
+last_gyro_x_angle = 0                       #Store the gyro angles to compare drift
+last_gyro_y_angle = 0
+last_gyro_z_angle = 0
+base_x_accel = 0                            #Calibrate acceleration sensor
+base_y_accel = 0
+base_z_accel = 0
+base_x_gyro = 0
+base_y_gyro = 0
+base_z_gyro = 0
+
+#DEFINE SETTER METHOD
+def set_last_read_angle_data(time,x,y,z,x_gyro,y_gyro,z_gyro): 
+  global last_read_time
+  last_read_time = time;
+  global last_x_angle
+  last_x_angle = x;
+  global last_y_angle
+  last_y_angle = y;
+  global last_z_angle
+  last_z_angle = z;
+  global last_gyro_x_angle
+  last_gyro_x_angle = x_gyro;
+  global last_gyro_y_angle
+  last_gyro_y_angle = y_gyro;
+  global last_gyro_z_angle
+  last_gyro_z_angle = z_gyro;
+
+
+#DEFINE GETTER METHOD
+def get_last_time():
+    return last_read_time
+def get_last_x_angle():
+    return last_x_angle
+def get_last_y_angle():
+    return last_y_angle
+def get_last_z_angle():
+    return last_z_angle
+def get_last_gyro_x_angle():
+    return last_gyro_x_angle
+def get_last_gyro_y_angle():
+    return last_gyro_y_angle
+def get_last_gyro_z_angle():
+    return last_gyro_z_angle
+
