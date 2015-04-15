89d318da01c69e7aeaee1ab349012dff646893a0
ali-abid <worktimee@gmail.com>
2015-04-15 14:23:11 +0100

    Convert MPU6050 Acc Gyro Raw Data

---
+++ convert_MPU6050_AccGyro_RawData.py
@@ -0,0 +1,7 @@
+# Convert MPU6050 accelerometer and gyroscope raw data into meaning full data.
+# And data stored into txt format, this file read by Processing software for visualize 
+# accelerometer and gyroscope movements. 
+#
+# Version 1.0 (March, 2015)
+# Abid Ali
+#IMaR Technology Gateway, Institute of Technology
