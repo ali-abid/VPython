197546d63dd532ea6c42e041daa25b40329815b7
ali-abid <worktimee@gmail.com>
2015-04-15 14:43:16 +0100

    Read Raw Data and Store Into Arrays

--- convert_MPU6050_AccGyro_RawData.py
+++ convert_MPU6050_AccGyro_RawData.py
@@ -87,3 +87,61 @@ def get_last_gyro_y_angle():
 def get_last_gyro_z_angle():
     return last_gyro_z_angle
 
+
+#DEFINE CALIBRATION METHOD
+#First 10 reading to compute the default sensor offset.
+#The offset get subtracted from raw data sensor values before the values are converted to angles.
+def calibrate_sensors(x,y,z,Gx,Gy,Gz):
+    num_readings = 10
+    x_accel = 0;
+    y_accel = 0;
+    z_accel = 0;
+    x_gyro = 0;
+    y_gyro = 0;
+    z_gyro = 0;
+    print("Starting Calibration")
+    for i in range(num_readings):
+        print(Tnum[i],Xnum[i],Ynum[i],Znum[i],GXnum[i],GYnum[i],GZnum[i])
+        rate(10)
+        x_accel += float(Xnum[i])
+        y_accel += float(Ynum[i])
+        z_accel += float(Znum[i])
+        x_gyro += float(GXnum[i])
+        y_gyro += float(GYnum[i])
+        z_gyro += float(GZnum[i])
+
+    x_accel /= num_readings
+    y_accel /= num_readings
+    z_accel /= num_readings
+    x_gyro /= num_readings
+    y_gyro /= num_readings
+    z_gyro /= num_readings
+
+    #Store the raw calibration values globally
+    global base_x_accel
+    base_x_accel = x_accel
+    global base_y_accel
+    base_y_accel = y_accel
+    global base_z_accel
+    base_z_accel = z_accel
+    global base_x_gyro
+    base_x_gyro = x_gyro
+    global base_y_gyro
+    base_y_gyro = y_gyro
+    global base_z_gyro
+    base_z_gyro = z_gyro;
+    print(base_x_accel,base_y_accel,base_z_accel,base_x_gyro,base_y_gyro,base_z_gyro)
+    print("Finishing Calibration")
+
+
+
+#READ RAW DATA FILE AND STORE INTO ARRAYS (Tnum, Xnum, Ynum, Znum, GXnum, GYnum, GZnum)
+RawDataFile = 'C:\Dev\workspace\VPython/golf-1-11-4.txt'
+file = open(RawDataFile, 'rU')
+reader = csv.reader(file)
+for line in reader:
+    reader.next() # This function make space in line
+    Tnum.append(line[0]),Xnum.append(line[1]),Ynum.append(line[2]),Znum.append(line[3]),GXnum.append(line[4]),GYnum.append(line[5]),GZnum.append(line[6])
+
+
+
